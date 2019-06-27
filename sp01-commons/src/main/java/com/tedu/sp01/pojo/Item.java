2e79799654432d4175c67fc4404648fc38cfd832
TEDU <TEDU@DESKTOP-4OPD87S>
2019-06-27 17:16:47 +0800

    加了个注释

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstruc
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
+	//id
 	private Integer id;
 	private String name;
 	private Integer number;
